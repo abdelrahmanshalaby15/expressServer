2da911e7963b0b0b295b7cdd770e4a0dfe2a5f7b
abdelrahmanshalaby15 <abdelrahman.shalaby@dentacarts.com>
2022-11-09 11:25:05 +0200

    ssss

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const port = 5555;
 app.get("/", (req, res) => {
   var host = req.get("host");
   console.log(req.headers);
-  res.send(`${req.originalUrl} - ${req.get("origin")} - ${host}`);
+  res.send(
+    `${req.originalUrl} - ${req.get("origin")} - ${host} - ${
+      req.socket.remoteAddress
+    } - ${req.headers["x-forwarded-for"]}`
+  );
 });
 
 app.listen(port, () => {
